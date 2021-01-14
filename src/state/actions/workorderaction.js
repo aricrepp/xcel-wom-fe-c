341c4e9abd5ad2aeb7e9aa850607af70a5be6d24
Ryan Schueck <ryschueck@gmail.com>
2021-01-13 22:25:54 -0500

    Fixed: Changed my workorderFailed to have no payload

--- src/state/actions/workorderaction.js
+++ src/state/actions/workorderaction.js
@@ -1,14 +1,16 @@
 const WORKORDER_SUBMITTED = 'WORKORDER_SUBMITTED';
 const FORM_FAILED = 'FORM_FAILED';
 
-export const workorderAction = formvalues => {
+const workorderAction = formvalues => {
   return dispatch => {
     dispatch({ type: WORKORDER_SUBMITTED, payload: formvalues });
   };
 };
 
-export const workorderFailed = err => {
+const workorderFailed = () => {
   return dispatch => {
-    dispatch({ type: FORM_FAILED, payload: err });
+    dispatch({ type: FORM_FAILED });
   };
 };
+
+export { workorderAction, workorderFailed };
